108b0bf1a1ef737f3c8c261bfc86ec019211e199
DmytroSerhatyi <dmytro.serhatyi@gmail.com>
2018-05-21 17:51:26 +0300

    solved task 172 - factorial trailing zeroes

---
+++ 151-200/172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function (n) {
+    var result = 0;
+
+    while (n >= 5) {
+        n = Math.floor(n / 5);
+        result += n;
+    }
+
+    return result;
+};
