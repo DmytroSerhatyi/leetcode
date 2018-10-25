3bca01f647c48e0b01a7cb6929a656dd86bc99e3
Dmytro Serhatyi <dmytro.serhatyi@gmail.com>
2018-10-25 23:18:08 +0300

    solved task 521 - longest uncommon subsequence i

---
+++ 501-550/521-longest-uncommon-subsequence-i/521-longest-uncommon-subsequence-i.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {number}
+ */
+var findLUSlength = function (a, b) {
+    return a === b
+        ? -1
+        : a.length > b.length
+            ? a.length
+            : b.length;
+};
