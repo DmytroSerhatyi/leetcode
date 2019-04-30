5140694af97540184cc4f9ec8dbcc28193bf7687
Dmytro Serhatyi <dmytro.serhatyi@gmail.com>
2019-04-30 14:14:03 +0300

    solved task 116 - populating next right pointers in each node

---
+++ 101-150/116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cs
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node(){}
+    public Node(int _val,Node _left,Node _right,Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+}
+*/
+public class Solution
+{
+    public Node Connect(Node root)
+    {
+        ConnectInternal(root, null);
+
+        return root;
+    }
+
+    private void ConnectInternal(Node current, Node next)
+    {
+        if (current == null) return;
+
+        current.next = next;
+
+        ConnectInternal(current.left, current.right);
+        ConnectInternal(current.right, next?.left);
+    }
+}
